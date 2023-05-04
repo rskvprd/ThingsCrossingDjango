ad33649188303f9875efde316161f448f78af430
rskvprd <vyackov2001@mail.ru>
2023-05-04 22:39:04 +0300

    really remove useless shit

--- ThingsCrossingDjango/urls.py
+++ ThingsCrossingDjango/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/v1/', include('rest_framework.urls', namespace='rest_framework')),
     path("form", views.form_test, name="form_test"),
-    path("__debug__", include('debug_toolbar.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
